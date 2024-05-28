b100e25af66366dcb89d085da8b0f161b2579757
rizaldiabyannata <aldizar255@gmail.com>
2024-05-28 17:48:57 +0800

    style: adjust font size

--- frontend/src/components/bannerHome.vue
+++ frontend/src/components/bannerHome.vue
@@ -15,19 +15,19 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
         </div>
-        <div class="font-Karantina font-bold text-xs text-black relative top-48 sm:text-base md:w-full">
+        <div class="font-Karantina font-bold text-xs text-black relative top-48 sm:text-base lg:text-lg md:w-full">
             <div
-                class="bg-geeks py-2 border-2 border-black flex justify-center items-center uppercase space-x-2 sm:space-x-4 md:place-items-center md:grid md:grid-cols-5 md:gap-2 md:px-20">
+                class="bg-geeks py-2 border-2 border-black flex justify-center items-center uppercase space-x-2 sm:space-x-4 md:place-items-center md:grid md:grid-cols-5 md:gap-2 md:px-20 lg:gap-4">
                 <div class="md:w-full">
                     <h1>Tanggal</h1>
                     <input type="date"
-                        class="border-2 border-black w-full bg-gray-300 text-sm font-normal py-1 pl-2 sm:text-base"
+                        class="border-2 border-black w-full bg-gray-300 text-sm font-normal py-1 pl-2 sm:text-base lg:text-lg"
                         v-model="tanggal">
                 </div>
                 <div class="md:w-full">
                     <h1>Waktu</h1>
                     <select v-model="jam"
-                        class="bg-gray-300 border-2 w-full border-black uppercase text-sm font-normal py-2 pl-2 sm:text-base">
+                        class="bg-gray-300 border-2 w-full border-black uppercase text-sm font-normal py-2 pl-2 sm:text-base lg:text-lg">
                         <option class="text-sm" value="Pagi">pagi</option>
                         <option class="text-sm" value="Siang">siang</option>
                         <option class="text-sm" value="Malam">malam</option>
@@ -36,7 +36,7 @@
                 <div class="md:w-full">
                     <h1>Keberangkatan</h1>
                     <select v-model="keberangkatan"
-                        class="bg-gray-300 border-2 w-full border-black uppercase text-sm font-normal py-2 pl-2 sm:text-base">
+                        class="bg-gray-300 border-2 w-full border-black uppercase text-sm font-normal py-2 pl-2 sm:text-base lg:text-lg">
                         <option value="lombok">lombok</option>
                         <option value="bima">Bima</option>
                     </select>
@@ -44,7 +44,7 @@
                 <div class="md:w-full">
                     <h1>Destinasi</h1>
                     <select v-model="destinasi"
-                        class="bg-gray-300 border-2 w-full border-black uppercase text-sm font-normal py-2 pl-2 sm:text-base">
+                        class="bg-gray-300 border-2 w-full border-black uppercase text-sm font-normal py-2 pl-2 sm:text-base lg:text-lg">
                         <option value="lombok">Lombok</option>
                         <option value="bima">Bima</option>
                     </select>
@@ -88,17 +88,18 @@
                 </div>
                 <div
                     class="relative -top-12 scale-75 sm:scale-90 md:scale-100 lg:scale-125 md:-top-4 lg:top-[5rem] md:pr-4 lg:pr-0 lg:right-12">
-                    <div class="bg-geeks w-48 sm:w-60 shadow-md p-4 text-sm lg:w-72">
+                    <div
+                        class="bg-geeks w-48 sm:w-60 shadow-md p-4 text-sm lg:w-80 sm:text-base lg:text-lg lg:space-y-2 lg:p-6">
                         <div class="lg:text-xl">
                             <h1>Tanggal</h1>
                             <input type="date"
-                                class="border-2 border-black bg-gray-300 w-full text-lg font-normal py-1 pl-2"
+                                class="border-2 border-black bg-gray-300 w-full text-sm font-normal py-1 pl-2 sm:text-base lg:text-lg"
                                 v-model="tanggal">
                         </div>
                         <div>
                             <h1 class="lg:text-xl">Waktu : </h1>
                             <select v-model="jam"
-                                class="w-full bg-gray-300 border-2 border-black uppercase text-sm font-normal py-1 pl-2">
+                                class="w-full bg-gray-300 border-2 border-black uppercase text-sm font-normal py-1 pl-2 sm:text-base lg:text-lg">
                                 <option value="Pagi">pagi</option>
                                 <option value="Siang">siang</option>
                                 <option value="Malam">malam</option>
@@ -107,7 +108,7 @@
                         <div>
                             <h1 class="lg:text-xl">Keberangkatan : </h1>
                             <select v-model="keberangkatan"
-                                class="w-full bg-gray-300 border-2 border-black uppercase text-lg font-normal py-1 pl-2">
+                                class="w-full bg-gray-300 border-2 border-black uppercase text-lg font-normal py-1 pl-2 sm:text-base lg:text-lg">
                                 <option value="lombok">lombok</option>
                                 <option value="bima">Bima</option>
                             </select>
@@ -115,7 +116,7 @@
                         <div>
                             <h1 class="lg:text-xl">Destinasi : </h1>
                             <select v-model="destinasi"
-                                class="w-full bg-gray-300 border-2 border-black uppercase text-lg font-normal py-1">
+                                class="w-full bg-gray-300 border-2 border-black uppercase text-lg font-normal py-1 pl-2 sm:text-base lg:text-lg">
                                 <option value="lombok">Lombok</option>
                                 <option value="bima">Bima</option>
                             </select>
@@ -123,7 +124,7 @@
                         <div class="pt-4">
                             <button @click="checkBis"
                                 class="bg-iPromise hover:bg-tentraO border-2 border-black px-6 py-1 transition-transform duration-300 ease-in-out transform hover:scale-110">
-                                <h1 class="lg:text-xl">Cari</h1>
+                                <h1 class="lg:text-2xl">Cari</h1>
                             </button>
                         </div>
                     </div>
